12083c4496e4c4cb14def780cf8115bfe6e65492
JNguyen93 <justinmnguyen1@gmail.com>
2015-10-22 16:24:49 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = args[0];
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+            var i = 2;
+            var str = "Hi";
+            var a = new[] { 1, 2, 3 };
+            //Sync please?!
+        }
+    }
+}
